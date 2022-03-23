dd312345ca103de543d2d3bff43f71da5e2fb8c5
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2022-03-23 14:10:06 +0530

    Added Separate JS for dates

--- main.js
+++ main.js
@@ -3,8 +3,25 @@ const aside = document.querySelector("aside");
 const asideTrigger = document.querySelector("#aside-trigger");
 const totalProgress = document.querySelector("#total-progress");
 const totalProgressContent = document.querySelector("#total-progress-content");
+// Dates
 const march22 = document.querySelector("#march-22");
 const march22content = document.querySelector("#march-22-content");
+const seventeenMarch22 = document.querySelector("#seventeen-march-22");
+const twentythreeMarch22 = document.querySelector("#twentythree-march-22");
+// Table
+const chetanPages = document.querySelector("#chetan-pages");
+const chetanEntry = document.querySelector("#chetan-entry");
+const chetanRemarks = document.querySelector("#chetan-remarks");
+const kishanPages = document.querySelector("#kishan-pages");
+const kishanEntry = document.querySelector("#kishan-entry");
+const kishanRemarks = document.querySelector("#kishan-remarks");
+const poukinPages = document.querySelector("#poukin-pages");
+const poukinEntry = document.querySelector("#poukin-entry");
+const poukinRemarks = document.querySelector("#poukin-remarks");
+const rohelPages = document.querySelector("#rohel-pages");
+const rohelEntry = document.querySelector("#rohel-entry");
+const rohelRemarks = document.querySelector("#rohel-remarks");
+
 
 totalProgressContent.classList.remove("hide");
 march22content.classList.add("hide");
@@ -24,4 +41,48 @@ totalProgress.addEventListener("click", () => {
 march22.addEventListener("click", () => {
   totalProgressContent.classList.add("hide");
   march22content.classList.remove("hide");
+  chetanPages.innerText = 0;
+  chetanEntry.innerText = 5;
+  chetanRemarks.innerText = "";
+  kishanPages.innerText = 448;
+  kishanEntry.innerText = 0;
+  kishanRemarks.innerText = "";
+  poukinPages.innerText = 320;
+  poukinEntry.innerText = 0;
+  poukinRemarks.innerText = "";
+  rohelPages.innerText = 0;
+  rohelEntry.innerText = 0;
+  rohelRemarks.innerText = "";
+})
+
+// Function to display the stats for 17 March 22
+seventeenMarch22.addEventListener("click", () => {
+  chetanPages.innerText = 0;
+  chetanEntry.innerText = 5;
+  chetanRemarks.innerText = "";
+  kishanPages.innerText = 448;
+  kishanEntry.innerText = 0;
+  kishanRemarks.innerText = "";
+  poukinPages.innerText = 320;
+  poukinEntry.innerText = 0;
+  poukinRemarks.innerText = "";
+  rohelPages.innerText = 0;
+  rohelEntry.innerText = 0;
+  rohelRemarks.innerText = "Absent";
+})
+
+// Function to display the stats for 23 March 22
+twentythreeMarch22.addEventListener("click", () => {
+  chetanPages.innerText = 0;
+  chetanEntry.innerText = 0;
+  chetanRemarks.innerText = "";
+  kishanPages.innerText = 0;
+  kishanEntry.innerText = 0;
+  kishanRemarks.innerText = "";
+  poukinPages.innerText = 0;
+  poukinEntry.innerText = 0;
+  poukinRemarks.innerText = "";
+  rohelPages.innerText = 0;
+  rohelEntry.innerText = 0;
+  rohelRemarks.innerText = "Absent";
 })
