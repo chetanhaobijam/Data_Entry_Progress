5ed85f2696c1e5fff049d676d798b138bfd91346
Haobijam Chetan Singh <67411987+chetanhaobijam@users.noreply.github.com>
2022-10-18 08:55:55 +0530

    Updated for 17 Oct 22

--- js/main.js
+++ js/main.js
@@ -201,6 +201,7 @@ const elevenOctober22 = document.querySelector("#eleven-october-22");
 const twelveOctober22 = document.querySelector("#twelve-october-22");
 const thirteenOctober22 = document.querySelector("#thirteen-october-22");
 const fourteenOctober22 = document.querySelector("#fourteen-october-22");
+const seventeenOctober22 = document.querySelector("#seventeen-october-22");
 
 const holiday = document.querySelectorAll(".holiday");
 const otherWork = document.querySelectorAll(".other-work");
@@ -1997,6 +1998,15 @@ async function getData() {
     let poukinScan = datas["2022"]["poukin"]["october"]["scan"][7];
     let rohelEntry = datas["2022"]["rohel"]["october"]["entry"][7];
     progress(0, 0, 0, 0, other, 0, 0, 0, chetanPrint, `Other Gazette Work; Print Completed for Book 1996-97`, 0, kishanEntry, 0, 0, `Other Gazette Work`, poukinScan, 0, 0, 0, em, 0, rohelEntry, 0, 0, other);
+  })
+
+  // Function to display the stats for 17 October 22
+  seventeenOctober22.addEventListener("click", () => {
+    let chetanEntry = datas["2022"]["chetan"]["october"]["entry"][8];
+    let chetanPdf = datas["2022"]["chetan"]["october"]["pdf"][8];
+    let poukinScan = datas["2022"]["poukin"]["october"]["scan"][8];
+    let rohelEntry = datas["2022"]["rohel"]["october"]["entry"][8];
+    progress(0, 0, 0, 0, other, 0, chetanEntry, chetanPdf, 0, em, 0, 0, 0, 0, abs, poukinScan, 0, 0, 0, em, 0, rohelEntry, 0, 0, other);
   })
 }
 
